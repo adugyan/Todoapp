7fde5936afaaf59e932238ca6faff755e6b7198c
adugyan <adugyan1602@gmail.com>
2020-07-08 14:53:19 -0500

    fetch update

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -18,11 +18,14 @@ db.create_all()
 #the route that listens to the form action /todos/create from index.html
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
-    description = request.form.get('description', '')
+    description = request.get_json()['description']
+    #create a todo item from the description value in index
     todo = Todo(description=description)
     db.session.add(todo)
     db.session.commit()
-    return redirect(url_for('index'))
+    return jsonify({
+     'description': todo.description
+    })
 
 @app.route('/')
 def index():
